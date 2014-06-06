34c9db76ad07d89906487c69c72db09ff172d14e
FabioBis <fabio.biselli.80@gmail.com>
2014-06-06 17:51:22 +0200

    Added attributes and  drafted constructor.

--- BoardGameCore/Connect4AlphaBetaStrategy.cs
+++ BoardGameCore/Connect4AlphaBetaStrategy.cs
@@ -25,6 +25,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpSearch;
 
 namespace BoardGameCore
 {
@@ -35,6 +36,60 @@ namespace BoardGameCore
     /// </summary>
     class Connect4AlphaBetaStrategy : BoardGameStrategy
     {
+        // The decision tree for the strategy.
+        private DecisionTree<Connect4Board> decisionTree;
+
+        // Integer value representing the player order:
+        //   if aiTurn = -1 the AI is the first to play, second to play
+        //   otherwise.
+        private int aiTurn;
+
+        /// <summary>
+        /// Constructor.
+        /// This constructor builds a new strategy based on the given AI turn.
+        /// if <code>turn</code> = -1 the IA is the first to play, second to
+        /// play otherwise.
+        /// </summary>
+        /// <param name="turn">The AI turn.</param>
+        public Connect4AlphaBetaStrategy(int turn)
+        {
+            if (turn == -1)
+            {
+                decisionTree = new DecisionTree<Connect4Board>(
+                    new Connect4Board(),
+                    new MinMaxDecision(null, MiniMax.Max)
+                    );
+                buildNode(decisionTree.GetRoot(), turn, MiniMax.Max);
+            }
+            else if (turn == 1)
+            {
+                decisionTree = new DecisionTree<Connect4Board>(
+                    new Connect4Board(),
+                    new MinMaxDecision(null, MiniMax.Min));
+                buildNode(decisionTree.GetRoot(), turn, MiniMax.Min);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            aiTurn = turn;
+        }
+
+        /// <summary>
+        /// This method build the given node, and recursively all the
+        /// children using depth-first traversal with pre-order visit.
+        /// </summary>
+        /// <param name="node">The node to be built.</param>
+        /// <param name="turn">The current turn.</param>
+        /// <param name="type">The MiniMax type of this node.</param>
+        private void buildNode(
+            DecisionTreeNode<Connect4Board> node,
+            int turn,
+            MiniMax type)
+        {
+            // TODO
+        }
+
         // TODO
         public override int OwnMove()
         {
